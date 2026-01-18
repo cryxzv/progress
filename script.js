8b429cd1a96b2ab7aa8566c2011fda9724d164bd
cryxzv <124369667+cryxzv@users.noreply.github.com>
2026-01-18 16:46:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -21,41 +21,98 @@ let txapr = textR[5]
 let txleap = textR[6]
 let startbtn = document.getElementById("start")
 
-function updateMin(){    
-    const d = new Date();
+function updateMin(d){    
     let seconds = d.getSeconds()
     evmin.style.width = (seconds/0.6).toString() + "%"
     txmin.innerHTML = (60-seconds).toString() + '</span>' + " Seconds Left"
-    console.log(txmin.innerHTML)
 }
 
-function updateHour(){
-    const d = new Date();
+function updateHour(d){
     let minutes = d.getMinutes()
     evhour.style.width = (minutes/0.6).toString() + "%"
     txhour.innerHTML = (60-minutes).toString() + " Minutes Left"
 }
 
-function updateTmr(){
-    const d = new Date();
+function updateTmr(d){
     let minutes = d.getMinutes()
     let hours = d.getHours()
     evtmr.style.width = ((hours*60 + minutes)/14.4).toString() + "%"
     txtmr.innerHTML = (24-hours).toString() + " Hours Left"
 }
 
-let start = false
+function updateMon(d){
+    let year = d.getFullYear()
+    let month = d.getMonth()
+    let totalDays = new Date(year, month, 0).getDate()
+    let days = d.getDate()
+    evmon.style.width = ((days*100)/totalDays).toString() + "%"
+    txmon.innerHTML = (totalDays-days).toString() + " Days Left"
+}
 
-startbtn.addEventListener('click', function(){
-    startbtn.style.scale = 0.08;
-    startbtn.style.top = "-6vh";
-    startbtn.style.backgroundColor = "#DEDEDE"
-    startbtn.style.border = "0.5rem #DEDEDE solid"
-    console.log(startbtn.style.scale)
-})
+function updateChr(d){
+    const chrYear = d.getFullYear()
+    if (d.getMonth() === 11 && d.getDate() > 25){
+        chrYear++
+    }
+    let chrDay = new Date(chrYear, 11, 25)
+    let timeDiff = chrDay.getTime() - d.getTime()
+    let dayDiff = Math.ceil(timeDiff/(1000*60*60*24))
+    evchr.style.width = ((31536000000-timeDiff)*100/(31536000000)).toString() + "%"
+    txchr.innerHTML = (dayDiff).toString() + " Days Left"
+}
+
+function updateApr(){
+    Math.random()
+    evapr.style.width = Math.floor((Math.random() * 100)).toString() + "%"
+    txapr.innerHTML = Math.floor((Math.random()*366)).toString() + " Days Left"
+}
+
+function updateLeap(d){
+    let leap = false
+    let year = d.getFullYear()
+    if (year % 100 == 0){
+        leap = false
+    } else if (year % 4 == 0) {
+        if (d.getTime > Date(year, 1, 29)){
+            leap = false
+        } else{
+            leap = true
+        }
+    }
+
+    while (!leap){
+        year++
+        if (year % 100 == 0){
+            leap = false
+        } else if (year % 4 == 0) {
+            leap = true
+        }
+    }
+
+    let leapDay = new Date(year, 1, 29)
+    let timeDiff = leapDay.getTime() - d.getTime()
+    let dayDiff = Math.ceil(timeDiff/(1000*60*60*24))
+    evleap.style.width = ((126230400000-timeDiff)*100/(126230400000)).toString() + "%"
+    txleap.innerHTML = (dayDiff).toString() + " Days Left"
+}
+
+// let start = false
+
+// startbtn.addEventListener('click', function(){
+//     startbtn.style.scale = 0.08;
+//     startbtn.style.top = "-6vh";
+//     startbtn.style.backgroundColor = "#DEDEDE"
+//     startbtn.style.border = "0.5rem #DEDEDE solid"
+//     console.log(startbtn.style.scale)
+// })
 
 setInterval(() => {
-    updateMin()
-    updateHour()
-    updateTmr()
+    const d = new Date();
+    updateMin(d)
+    updateHour(d)
+    updateTmr(d)
+    updateMon(d)
+    updateChr(d)
+    updateApr()
+    updateLeap(d)
 }, 1000);
